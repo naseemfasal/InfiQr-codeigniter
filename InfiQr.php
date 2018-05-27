39e31e18147d3ff99b1c2cfa1904108919770d4c
Naseem Fasal <naseem770@gmail.com>
2018-05-27 06:26:20 +0300

    new code generated

---
+++ InfiQr.php
@@ -0,0 +1,27 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * InfiQr Codeigniter Qr Code generator Library
+ *
+ * Generate Qr code in your CodeIgniter applications.
+ *
+ * @package			CodeIgniter
+ * @subpackage		Libraries
+ * @category		Libraries
+ * @author			Naseem Fasal
+ * @license			None
+ * @link			https://github.com/naseemfasal
+ */
+
+require_once(dirname(__FILE__) . '/InfiQr/qrlib.php');
+
+class Qrcodegen 
+{
+
+
+	public function generate($conents,$type)
+	{
+		    QRcode::$type($conents); 
+
+	}
+}
